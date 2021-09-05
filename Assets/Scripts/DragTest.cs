ba70fb4de47baf017e5f87a9bfb212d2f8846abb
SuperAruba <437895304@qq.com>
2021-09-05 22:54:03 +0800

    加模型和串口功能

---
+++ Assets/Scripts/DragTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTest : MonoBehaviour
+{
+    private Vector3 screenPos;
+    private Vector3 offset;
+    void OnMouseDown()
+    {
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);//获取物体的屏幕坐标     
+        offset = screenPos - Input.mousePosition;//获取物体与鼠标在屏幕上的偏移量    
+    }
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + offset);//将拖拽后的物体屏幕坐标还原为世界坐标
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+
+public class UIManager : MonoBehaviour
+{
+    public string portName = "COM2";//串口号
+    public int baudRate = 9600;//波特率
+    public Parity parity = Parity.None;//效验位
+    public int dataBits = 8;//数据位
+    public StopBits stopBits = StopBits.One;//停止位
+    SerialPort sp = null;
+    public List<byte> listReceive = new List<byte>();
+    char[] strchar = new char[100];//接收的字符信息转换为字符数组信息
+    string str;
+
+    void Start()
+    {
+        OpenPort();
+    }
+    public void OpenPort()
+    {
+
+        //创建串口
+        sp = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+        sp.ReadTimeout = 400;
+        try
+        {
+            sp.Open();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+            Debug.Log(portName);
+            Debug.Log(baudRate);
+            Debug.Log(parity);
+            Debug.Log(dataBits);
+            Debug.Log(stopBits);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        ClosePort();
+    }
+
+    public void ClosePort()
+    {
+        try
+        {
+            sp.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    private void Update()
+    {
+        //PrintData();
+
+    }
+
+    //打印数据
+    void PrintData()
+    {
+        for (int i = 0; i < listReceive.Count; i++)
+        {
+            strchar[i] = (char)(listReceive[i]);
+            str = new string(strchar);
+        }
+        Debug.Log(str);
+
+    }
+
+    //接收数据
+    void DataReceiveFunction()
+    {
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+        while (true)
+        {
+            if (sp != null && sp.IsOpen)
+            {
+                try
+                {
+                    bytes = sp.Read(buffer, 0, buffer.Length);//接收字节
+                    if (bytes == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string strbytes = Encoding.Default.GetString(buffer);
+                        //接受的数据
+                        Debug.Log(strbytes);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex.GetType() != typeof(ThreadAbortException))
+                    {
+                    }
+                }
+            }
+            Thread.Sleep(10);
+        }
+    }
+
+    //发送数据
+    public void WriteData(string dataStr)
+    {
+        Debug.Log(dataStr);
+        if (sp.IsOpen)
+        {
+            sp.Write(dataStr);
+
+        }
+    }
+}
