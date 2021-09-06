a878318efcb830e409caab83838fb78d074b53a7
SuperAruba <437895304@qq.com>
2021-09-07 00:53:25 +0800

    增加吸附脚本

---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    public Vector3 FixedPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,FixedPosition) < 20)
+        {
+            transform.position = Vector3.Lerp(transform.position, FixedPosition, 1);
+        }
+        
+    }
+}
